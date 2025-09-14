bbeb91b17e9de6dcfc6055c625f4f8297dfc96c5
harshkulyal9 <harshvardhankulyal0@gmail.com>
2025-09-14 19:50:19 +0530

    Add files via upload

---
+++ fine.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int latedays;
+    printf("enter the late days: ");
+    scanf("%d",&latedays);
+    int charges;
+    int fine;
+    fine=latedays*charges;
+
+    if(latedays=0){
+        printf("no fine");
+    }
+else if(latedays>0 && latedays<=5)
+    
+
+}
